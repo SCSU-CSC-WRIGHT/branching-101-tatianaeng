a073eb256fbb6d327cd0779e7588cf70a47e5dea
Tatiana Eng <tatianaeng@mac.lan>
2025-02-15 15:13:26 -0500

    Corrected code in main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 """There are 3 errors in this code. Please use your own ability to find them all. Consider this a Python refresher."""
 
+# initialize total to 0
+total = 0
+
 for i in range(5):
-    number = input("Enter a number: ")
+
+    # make this input an int
+    number = int(input("Enter a number: "))
+    
     total += number
 
-print("The running total is: "  total)
+# add a comma after the string literal
+print("The running total is: ", total)
